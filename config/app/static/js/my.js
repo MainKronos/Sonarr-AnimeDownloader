97db11637542e4f7949c1856b3f489c6af37c569
Krónos <lorenzo.chesi@live.it>
2021-01-19 17:28:16 +0100

    FeatureDiscovery
    aggiunto il FeatureDiscovery al pulsante principale

--- config/app/static/js/my.js
+++ config/app/static/js/my.js
@@ -1,21 +1,32 @@
-M.AutoInit();
+// M.AutoInit();
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.collapsible');
-//     var instances = M.Collapsible.init(elems, {
-//     	accordion: true
-//     });
-// });
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, {
+    	accordion: true
+    });
+});
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.tabs');
-//     var instances = M.Tabs.init(elems, {
-//     	swipeable: false
-//     });
-// });
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tabs');
+    var instances = M.Tabs.init(elems, {
+    	swipeable: false
+    });
+});
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.modal');
-//     var instances = M.Modal.init(elems, {
-//     });
-// });
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tap-target');
+    var instances = M.TapTarget.init(elems, {
+
+    });
+    var elem = document.querySelector('.tap-target');
+    var instance = M.TapTarget.getInstance(elem);
+    instance.open()
+    setTimeout(function(){ instance.close();  instance.destroy();}, 3000);
+});
