a2a24e98dca3ca05a236b2c07c96987f944778a1
Krónos <lorenzo.chesi@live.it>
2022-02-12 21:36:48 +0100

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-res = requests.get("http://192.168.1.11:8989/api/wanted/missing?apikey=f10994e9f3494368a31a3088aba6b9fc&sortKey=airDateUtc&page=0")
-
-
-print(res.json()["records"][0]["series"]["tvdbId"])
-
-# print(json.dumps(res.json(), indent=2))
